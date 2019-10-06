db073334c684cc5e237b44d47abd8a8adc040f35
Thiago Azevedo <thiago7azevedo@gmail.com>
2019-10-06 16:54:41 -0300

    ComboBox JavaFX

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,57 +1,72 @@
 package gui;
 
 import java.net.URL;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import gui.util.Alerts;
-import gui.util.Constraints;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import model.entities.Person;
 
-public class ViewController implements Initializable { // implementa a interface initialazable para utilizar o public void initialize
+public class ViewController implements Initializable { // implementa a interface initialazable para utilizar o public
+														// void initialize
 
 	@FXML
-	private TextField txtNumber1; //o campo o texto no SceneBiulder
-	@FXML
-	private TextField txtNumber2; //o campo o texto no SceneBiulder
+	private ComboBox<Person> comboBoxPerson; //comboBox é a caixa de texto para escolher opções em SceneBiulder da classe Person
 	
 	@FXML
-	private Label labelResult;//o campo o texto no SceneBiulder
-	
-	
-	@FXML
-	private Button btSum; //o campo botão no SceneBiulder
+	private Button btAll;
+
+	private ObservableList<Person> obsList; // aqui ele trata junto com o comboBox, atrelando a classe Person
 	
 	@FXML
-	public void onBtSumAction() {
-		try {
-			
-				Locale.setDefault(Locale.US);
-				double number1 = Double.parseDouble(txtNumber1.getText()); // passa o atributo numero de texto para double
-				double number2 = Double.parseDouble(txtNumber2.getText());// passa o atributo numero de texto para double
-				double sum = number1 + number2; // soma os atributos recebdos e convertidos
-				labelResult.setText(String.format("%.2f", sum)); // mostra o valor da soma setando uym texto no valor lebelresult
-				//joga para o SceneBiulder o valor da soma setando em texto o resultado
-		}
-		catch (NumberFormatException e) {
-			Alerts.showAlert("Error", "Parse Error", e.getMessage(), AlertType.ERROR); // captura o erro e mostra em um showAlert
-			//titulo, cabeçalo, msg...
+	public void onComboBoxPersonAction() {
+		Person person = comboBoxPerson.getSelectionModel().getSelectedItem();
+		System.out.println(person);
+	}
+	
+	@FXML
+	public void onBtAllAction() {
+		for (Person person : comboBoxPerson.getItems()) {
+		System.out.println(person);
 		}
 	}
+	
 
 	@Override
-	public void initialize(URL url, ResourceBundle rb) { // url =  caminho da tela
-		Constraints.setTextFieldDouble(txtNumber1); // Constraints é a classe nova criada, chamada quando o contolador é chamado
-		Constraints.setTextFieldDouble(txtNumber2); // set que somente numeros double podem ser escritos, nem letras
-		Constraints.setTextFieldMaxLength(txtNumber1, 12); // aqui seta no máximo 12 campos
-		Constraints.setTextFieldMaxLength(txtNumber2, 12); // aqui seta no máximo 12 campos
+	public void initialize(URL url, ResourceBundle rb) { // url = caminho da tela
+
+		List<Person> list = new ArrayList<>(); // cria litsa onde vai entrar os objetos de Person
+		list.add(new Person(1, "Maria", "maria@gmail.com"));
+		list.add(new Person(2, "Alex", "Alex@gmail.com"));
+		list.add(new Person(3, "Bob", "bob@gmail.com"));
+
+		obsList = FXCollections.observableArrayList(list); // coloca em ObservableList a lista criada, com ajuda da FXCollecions
+		comboBoxPerson.setItems(obsList); //seta os items da lista e da obsList em comboBox
 		
+		
+//---------------------------------------------------------------------------------------------------
+		
+		//abaixo um método para mostrar somente os nomes das pessoas dentro do comboBox
+		
+		Callback<ListView<Person>, ListCell<Person>> factory = lv -> new ListCell<Person>() {
+			@Override
+			protected void updateItem(Person item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getName());
+			}
+		};
+		comboBoxPerson.setCellFactory(factory);
+		comboBoxPerson.setButtonCell(factory.call(null));
+
 	}
-	
 
 }
